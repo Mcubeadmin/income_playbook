a8d624c6ac4200688f55b8503b7335e19e6a4a18
Mcubeadmin <mcubemujahid@gmail.com>
2025-11-14 18:05:55 +0530

    Added description filter functions

--- playbook.py
+++ playbook.py
@@ -3,7 +3,7 @@
 
 # # Importing Data
 
-# In[22]:
+# In[1]:
 
 
 # Remove the last row from the DataFrame
@@ -18,7 +18,7 @@ import plotly.express as px
 
 # ## Test set
 
-# In[ ]:
+# In[2]:
 
 
 df = pd.read_csv('statements/1763033905327Uc7bP9NWfoP6e7u2.xls', skiprows=20, delimiter='\t', engine='python')
@@ -37,7 +37,7 @@ print('End date of data:', edate.strftime('%-d %b %Y'))
 df
 
 
-# In[2]:
+# In[3]:
 
 
 plt.figure(figsize=(12, 6))
@@ -53,7 +53,7 @@ plt.show()
 
 # # Full Data
 
-# In[20]:
+# In[17]:
 
 
 all_files = glob.glob(os.path.join('statements', '*.xls'))
@@ -73,6 +73,7 @@ for file in all_files:
 # Concatenate all DataFrames and sort by date
 full_df = pd.concat(dfs, ignore_index=True)
 full_df = full_df.sort_values('Txn Date').reset_index(drop=True)
+full_df["Value Date"] = full_df["Txn Date"].dt.year * 100 + full_df["Txn Date"].dt.month
 duplicates = full_df[full_df.duplicated(keep=False)]
 sdate, edate = full_df['Txn Date'].iloc[0], full_df['Txn Date'].iloc[-1]
 print('Start date of data:', sdate.strftime('%-d %b %Y'))
@@ -83,7 +84,7 @@ print(f'Duplicate(s) removed: {duplicates.shape[0]}')
 full_df
 
 
-# In[21]:
+# In[5]:
 
 
 plt.figure(figsize=(15, 6))
@@ -98,7 +99,7 @@ plt.tight_layout()
 plt.show()
 
 
-# In[24]:
+# In[6]:
 
 
 fig = px.scatter(
@@ -118,18 +119,23 @@ fig.show()
 
 # # Monthly analysis
 
-# In[41]:
+# In[120]:
 
 
-month, year = 10, 2025
-selected_data = full_df[full_df['Txn Date'].dt.month == month]
-selected_data = selected_data[selected_data['Txn Date'].dt.year == year]
+start_month, end_month = 2, 11
+start_year, end_year = 2025, 2025
+
+start = start_year * 100 + start_month   # e.g., 2020*100 + 1 = 202001
+end   = end_year * 100 + end_month       # e.g., 2022*100 + 6 = 202206
+
+selected_data = full_df[full_df["Value Date"].between(start, end)]
+
 fig = px.scatter(
     selected_data,
     x='Txn Date',
     y='Balance',
     hover_data=['Description', 'Debit', 'Credit'],  # Replace 'Description' with your actual column name
-    title=f"{selected_data['Txn Date'].dt.strftime('%B %Y').iloc[0]} Transactions",
+    title=f"Date Range: {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[0]} - {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[-1]}",
     labels={'Txn Date': 'Transaction Date', 'Balance': 'Balance'},
     width=1000,
     height=500
@@ -137,18 +143,13 @@ fig = px.scatter(
 fig.update_traces(mode='lines+markers', marker=dict(size=5))
 fig.update_layout(xaxis_tickangle=90)
 fig.show()
-selected_data.sort_values(by='Debit', ascending=False).head(10)
-
-
-# In[42]:
-
 
 fig = px.scatter(
     selected_data,
     x='Txn Date',
     y='Debit',
     hover_data=['Description'],  # Replace 'Description' with your actual column name
-    title=f"{selected_data['Txn Date'].dt.strftime('%B %Y').iloc[0]}",
+    title=f"Debits: {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[0]} - {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[-1]}",
     labels={'Txn Date': 'Transaction Date', 'Balance': 'Balance'},
     width=1000,
     height=500
@@ -157,17 +158,45 @@ fig.update_traces(mode='markers', marker=dict(size=5))
 fig.update_layout(xaxis_tickangle=90)
 fig.show()
 
+fig = px.scatter(
+    selected_data,
+    x='Txn Date',
+    y='Credit',
+    hover_data=['Description'],  # Replace 'Description' with your actual column name
+    title=f"Credits: {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[0]} - {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[-1]}",
+    labels={'Txn Date': 'Transaction Date', 'Balance': 'Balance'},
+    width=1000,
+    height=500
+)
+fig.update_traces(mode='markers', marker=dict(size=5))
+fig.update_layout(xaxis_tickangle=90)
+fig.show()
 
-# In[58]:
+print(f"Date Range: {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[0]} - {selected_data['Txn Date'].dt.strftime('%B %Y').iloc[-1]}")
+print('Top 10 Debits:')
+selected_data.sort_values(by='Debit', ascending=False).head(10)
 
 
-filter_df = full_df[full_df['Description'].str.contains('zerodhabro')]
-print(f'Total Amount debited: {filter_df['Debit'].sum()}')
-filter_df
+# In[116]:
 
 
-# In[ ]:
+import num2words
+
+def to_words(amount):
+    words = num2words.num2words(amount, lang='en', to='ordinal')
+    return words
+
+def filter_search(df, phrases):
+    filter_df = df[df['Description'].str.contains('|'.join(phrases))]
+    print(f'Search Phrase: {phrases}')
+    debit_value, credit_value = filter_df['Debit'].sum(), filter_df['Credit'].sum()
+    print(f'Total Amount debited: {debit_value} ({to_words(int(debit_value))}')
+    print(f'Total Amount Credited: {credit_value} ({to_words(int(credit_value))})')
+    return filter_df
 
 
+# In[131]:
 
+
+filter_search(selected_data, ['hungerbox'])
 
